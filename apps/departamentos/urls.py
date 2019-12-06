33cb27b64dfcfb4cd9ee37956d7dc347b5ae66f1
fernando.sferreira <fernando.sferreira@sonda.com>
2019-12-06 13:12:04 -0300

    implementação das view de Departamentos.

---
+++ apps/departamentos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    DepartamentoList,
+    DepartamentoNovo,
+    DepartamentoEdit,
+    DepartamentoDelete
+)
+
+urlpatterns = [
+    path('', DepartamentoList.as_view(), name='list_departamentos'),
+    path('novo/', DepartamentoNovo.as_view(), name='create_departamento'),
+    path('editar/<int:pk>', DepartamentoEdit.as_view(), name='update_departamento'),
+    path('delete/<int:pk>', DepartamentoDelete.as_view(), name='delete_departamento'),    
+]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
+from .models import Departamento
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class DepartamentoList(ListView):
+    model = Departamento
+
+class DepartamentoNovo(CreateView):
+    model = Departamento
+    fields = ('nome',)
+
+class DepartamentoEdit(UpdateView):
+    model = Departamento
+    fields = ('nome',)
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
