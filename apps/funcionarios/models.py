2229ffe47743fbf71e75f55018c00ca426334107
FernandoDevBh <fsffernando2@gmail.com>
2019-12-05 21:25:06 -0300

    Criação do crud funcionário

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
@@ -8,7 +9,11 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
+    
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
+from functools import reduce
+from django.contrib.auth.models import User
+from .models import Funcionario
+from django.urls import reverse_lazy
 
-def home(request):
-    return HttpResponse('Olá')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        return queryset
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ('nome', 'departamentos')
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
+
+class FuncionarioNovo(CreateView):
+    model = Funcionario
+    fields = ('nome', 'departamentos')
+
+    def form_valid(self, form): 
+        funcionario = form.save(commit=False)
+        funcionario.empresa = self.request.user.funcionario.empresa
+        username = reduce(lambda x, y: x+y ,funcionario.nome.split(' '))
+        funcionario.user = User.objects.create_user(username=username)
+        funcionario.save()
+        return super().form_valid(form)
