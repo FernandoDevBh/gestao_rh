2e77cfd548214da2e9d918794592551ce6bb63e7
FernandoDevBh <fsffernando2@gmail.com>
2019-12-06 19:10:06 -0300

    inclusão do form bootstrap e criação do arquivo requirements.txt

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -11,11 +11,22 @@ class DepartamentoView():
     model = Departamento
     fields = ('nome',)
 
+    def get_empresa(self):
+        return self.request.user.funcionario.empresa
+
 class DepartamentoList(DepartamentoView, ListView):
-    pass
+    
+    def get_queryset(self):
+        empresa = self.get_empresa()
+        return Departamento.objects.filter(empresa=empresa)
 
 class DepartamentoNovo(DepartamentoView, CreateView):
-    pass
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.get_empresa()
+        departamento.save()
+        return super().form_valid(form)
 
 class DepartamentoEdit(DepartamentoView, UpdateView):
     pass
