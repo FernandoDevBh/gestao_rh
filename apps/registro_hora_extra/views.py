39ca8da6c0b5c5bff94b61be9908a9220b2139ef
fernando.sferreira <fernando.sferreira@sonda.com>
2019-12-09 10:49:28 -0300

    Inclusão da classe forms.

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -6,21 +6,34 @@ from django.views.generic import (
 )
 from .models import RegistroHoraExtra
 from django.urls import reverse_lazy
+from .forms import RegistroHoraExtraForm
 
-class RegistroHoraExtraView():
+
+class RegistroHoraExtraList(ListView):
     model = RegistroHoraExtra
-    fields = ('motivo','funcionario','horas')
+    form_class = RegistroHoraExtraForm
 
-class RegistroHoraExtraList(RegistroHoraExtraView, ListView):
     def get_queryset(self):
         empresa = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa)
 
-class RegistroHoraExtraNovo(RegistroHoraExtraView, CreateView):
-    pass    
+class RegistroHoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
 
-class RegistroHoraExtraEdit(RegistroHoraExtraView, UpdateView):
-    pass    
+    def get_form_kwargs(self):
+        kwargs = super(RegistroHoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({ 'user': self.request.user })
+        return kwargs
 
-class RegistroHoraExtraDelete(RegistroHoraExtraView, DeleteView):    
+class RegistroHoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(RegistroHoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({ 'user': self.request.user })
+        return kwargs
+
+class RegistroHoraExtraDelete(DeleteView):    
     success_url = reverse_lazy('list_registros_hora_extra')
