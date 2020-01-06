d21fc2112521163589280acec4f02e5f261315b1
FernandoDevBh <fsffernando2@gmail.com>
2020-01-06 19:51:29 -0300

    inclusão do local_settings

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n5t8v=%zxt%3yn2_pa@b33smwkf9lbe$0xp^h#1jy+g^y)$x_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,19 +80,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gestao_rh.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'antigo': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'dbantido.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -153,17 +138,13 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 CELERY_CACHE_BACKEND = 'django-cache'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+
 CELERY_ACEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
 
-EMAIL_USE_TLS = True
 
 DATABASE_ROUTERS = ['gestao_rh.DBRoutes.DBRoutes']
+
+from .local_settings import *
